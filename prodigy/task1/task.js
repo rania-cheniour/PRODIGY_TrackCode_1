6c222e14828b1c6495596829017b70f487dbfa7a
rania-cheniour <160713986+rania-cheniour@users.noreply.github.com>
2024-05-10 15:25:06 +0100

    Add files via upload

---
+++ prodigy/task1/task.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navigation');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 20) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
